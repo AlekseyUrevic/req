d0d29b4b34a3d639b7464d72609ec1a3cbc9906e
Aleksey <engen.alexey@yandex.ru>
2024-02-27 22:26:50 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using static WorkWithString;
+using static AdditionalMethods;
+
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintAboutProgram(File.ReadAllText(@"HelloUser.md"));
+
+        string[] startArray = CreateArrayString();
+        Console.Clear();
+        PrintArrayString(startArray);
+
+        string[] modificateArray = ModificateArray(startArray);
+        Console.Clear();
+        PrintArrayString(startArray, "Ваш исходный массив: ");
+        PrintArrayString(modificateArray, "Ваш модифицированный массив: ");
+
+
+    }
+}
